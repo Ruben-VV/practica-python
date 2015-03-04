58f65da35ddb64764ef5f79a35f4a8a201a4d537
Estudiante en practicas <pn23@noreste.inm.es>
2015-03-04 13:43:50 +0000

    -

--- Python basico/ejemplo_5.11.py
+++ Python basico/ejemplo_5.11.py
@@ -21,7 +21,12 @@ seeded = [49,4,18,26,29,9,16,12,2,22,10,34]
 unseeded = [61,33,62,45,0,30,82,10,20,358,63,NaN]
 strikes = pd.DataFrame(seeded,columns=['seeded'])
 strikes['unseeded'] = unseeded
-strikes.loc[11,'unseeded']=None
+##strikes.loc[11,'unseeded']=None
+##seeded = [49,4,18,26,29,9,16,12,2,22,10,34]
+##unseeded = [61,33,62,45,0,30,82,10,20,358,63,NaN]
+####strikes = pd.DataFrame(array([seeded, unseeded]).T,columns=['seeded', 'unseeded'])
+##strikes = pd.DataFrame(array([seeded, unseeded])).T
+##strikes.columns = ['seeded', 'unseeded']
 
 print strikes
 
